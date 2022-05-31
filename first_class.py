3e7482d3454a1b81490639d22b89141a170fb93e
Arshak <arshakaraqelyan91@gmail.com>
2022-05-31 20:47:12 +0400

    Added first_class.py

---
+++ first_class.py
@@ -0,0 +1,101 @@
+# sample_str = '''string 
+
+# sample_str = "string value"
+# sample_str = 'string value'
+
+# sample_str = """string 
+
+
+
+# value"""
+
+# value'''
+# a = 1 + 2 + 3
+
+# print(sample_str, a)
+
+sample_str = "string value"
+
+comp_number = 1 + 2j
+
+float_number = 5.5
+
+number = 8
+
+sample_bool = True
+sample_bool_2 = False
+
+
+
+# # print(type(sample_str))
+# # type_of_sample_string = type(sample_str)
+# # print(type_of_sample_string)
+# # print(type(float_number))
+# # print(type(sample_bool))
+# # print(type(comp_number))
+
+# print(type("5.5"))
+
+
+# result = sample_str + " " +str(float_number)
+
+# # print(result)
+
+# print(int(number + float_number))
+# value = "6"
+# print(int(value) + 8)
+
+value = -6
+zero = 0
+# print(bool(value), bool(zero))
+
+# print("*" * 0)
+# print("*" * False)
+# print("*" * True)
+
+num_1 = 2
+num_2 = 4
+
+# print(num_1 ** num_2)
+
+# print(10 / 20)
+# print(10 % 20)
+# print(10 // 20)
+
+# print(complex("2+3j"))
+
+# num_1 = num_1 + num_2
+# # or 
+# num_1 += num_2
+
+# num_1 = num_1 - num_2
+# # or
+# num_1 -= num_2
+
+# num_1 -= num_2 + 6 + 8
+
+# num_1 = num_1 // num_2
+# # or
+# num_1 //= num_2
+
+# print(num_1)
+
+
+number_3 = 5
+
+is_even_reminder = number_3 % 2  # False
+# print(is_even)
+
+# print("It is even" * bool(is_even_reminder - 1), end="")
+# print("It is odd" * bool(is_even_reminder), end="")
+
+## or 
+print("It is even" * bool(is_even_reminder - 1), "It is odd" * bool(is_even_reminder), sep="")
+
+
+
+
+
+
+
+
