df7d753f05b4110f5c9478d6c7dec98b5f7446ab
ArshakAraqelyan <106613724+ArshakAraqelyan@users.noreply.github.com>
2022-05-31 21:00:18 +0400

    Update HW9.py

--- HW9.py
+++ HW9.py
@@ -23,8 +23,8 @@ print("updated from github")
 #         print("No common elements")
 #
 #
-# a = (1, 2, 3, 4, 5)
-# b = (5, 6, 7, 8, 9)
+a = (1, 2, 3, 4, 5)
+b = (5, 6, 7, 8, 9)
 # common_member(a, b)
 
 
